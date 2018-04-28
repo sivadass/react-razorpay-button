d8e5d89b8ac9f91dfdb39099d8a1cf25273efc0a
Sivadass Navaneethan <nsivadass@gmail.com>
2018-04-28 22:35:50 +0530

    Added onClick handler for button

--- src/components/razorpay-button.js
+++ src/components/razorpay-button.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 class RazorpayButton extends React.Component {
+  
+  // Submit payment on click
+  handleClick = (params) => {
+    this.props.razorPaySubmit(params);
+  }
+  
   render() {
     return (
       <div className="razorpay-button-wrapper">
-        <button>Checkout with Razorpay</button>
+        <button onClick={this.handleClick}>Checkout with Razorpay</button>
       </div>
     );
   }
