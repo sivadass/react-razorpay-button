ff474023d7f4de642ebe990d8bfcbb8f4e7e1440
Sivadass Navaneethan <nsivadass@gmail.com>
2018-05-04 23:24:31 +0530

    Added dynamic  className API

--- src/components/razorpay-button.js
+++ src/components/razorpay-button.js
@@ -9,7 +9,7 @@ class RazorpayButton extends React.Component {
   
   render() {
     return (
-      <div className="razorpay-button-wrapper">
+      <div className={"razorpay-button-wrapper"+" "+this.props.className}>
         <button style={{width: "100%", background: "#f5f5f5", borderRadius: "4px"}} onClick={this.handleClick}>Checkout with Razorpay</button>
       </div>
     );
