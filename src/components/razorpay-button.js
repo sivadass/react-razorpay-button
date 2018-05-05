61ee1adf45ffe0e5706e82086aaf8de1bfaf1698
Sivadass N <nsivadass@gmail.com>
2018-05-05 23:49:30 +0530

    Added active status for modal

--- src/components/razorpay-button.js
+++ src/components/razorpay-button.js
@@ -1,16 +1,30 @@
 import React from "react";
 
 class RazorpayButton extends React.Component {
-  
   // Submit payment on click
-  handleClick = (params) => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false
+    };
+  }
+
+  handleClick(params) {
+    this.setState({
+      isActive: true
+    });
     this.props.razorPaySubmit(params);
   }
-  
+
   render() {
     return (
-      <div className={"razorpay-button-wrapper"+" "+this.props.className}>
-        <button style={{width: "100%", background: "#f5f5f5", borderRadius: "4px"}} onClick={this.handleClick}>Checkout with Razorpay</button>
+      <div className={"razorpay-button-wrapper" + " " + this.props.className}>
+        <button
+          style={{ width: "100%", background: "#f5f5f5", borderRadius: "4px" }}
+          onClick={this.handleClick}
+        >
+          Checkout with Razorpay
+        </button>
       </div>
     );
   }
