0c90129f5939bc63d6a37128043bf8e1cbe17d72
Sivadass N <nsivadass@gmail.com>
2018-05-06 23:35:06 +0530

    Addded razor pay configurations

--- src/components/razorpay-button.js
+++ src/components/razorpay-button.js
@@ -5,15 +5,34 @@ class RazorpayButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isActive: false
+      isActive: false,
+      options: {
+        key: "YOUR_KEY_ID",
+        amount: "2000", // 2000 paise = INR 20
+        name: "Merchant Name",
+        description: "Purchase Description",
+        image: "/your_logo.png",
+        handler: function(response) {
+          alert(response.razorpay_payment_id);
+        },
+        prefill: {
+          name: "Harshil Mathur",
+          email: "harshil@razorpay.com"
+        },
+        notes: {
+          address: "Hello World"
+        },
+        theme: {
+          color: "#F37254"
+        }
+      }
     };
   }
 
-  handleClick(params) {
-    this.setState({
-      isActive: true
-    });
-    this.props.razorPaySubmit(params);
+  handleClick(e) {
+    let razorPay = new Razorpay(this.state.options);
+    razorPay.open();
+    e.preventDefault();
   }
 
   render() {
